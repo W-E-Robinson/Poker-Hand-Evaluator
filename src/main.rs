891133522b21a1942bb05240b9f82ef8b50e930c
Will Robinson <william.ellis.robinson@gmail.com>
2024-08-24 23:51:03 +0100

    feat: added initial web server to serve GET /variants and serve 404 if method not found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("API Service");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap(); // NOTE: irrecoverable
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap(); // NOTE: recoverable
+
+    let (status_line, filename) = if request_line == "GET /variants HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "./src/assets/variants.json")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "./src/assets/404_not_found.json")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap(); // NOTE: recoverable
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap(); // NOTE: recoverable
 }
