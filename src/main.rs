c3628ea3ec4c7f55e25ae07ab134cceb59753312
Will Robinson <william.ellis.robinson@gmail.com>
2024-08-26 22:17:49 +0100

    feat: updated main rs handle connection error handling

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,32 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap(); // NOTE: irrecoverable
+    let listener = match TcpListener::bind("127.0.0.1:8080") {
+        Ok(tcp_listener) => tcp_listener,
+        Err(error) => panic!("Problem starting the TcpLister: {:?}", error),
+    };
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
+        match stream {
+            Ok(stream) => handle_connection(stream),
+            Err(error) => eprintln!("Error accepting connection: {:?}", error),
+        }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap(); // NOTE: recoverable
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(error)) => {
+            eprintln!("Failed to read line: {}", error);
+            return;
+        }
+        None => {
+            eprintln!("Connetion closed unexpectedly");
+            return;
+        }
+    };
 
     let (status_line, filename) = if request_line == "GET /variants HTTP/1.1" {
         ("HTTP/1.1 200 OK", "./src/assets/variants.json")
@@ -24,10 +38,19 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND", "./src/assets/404_not_found.json")
     };
 
-    let contents = fs::read_to_string(filename).unwrap(); // NOTE: recoverable
+    let contents = match fs::read_to_string(filename) {
+        Ok(line) => line,
+        Err(error) => {
+            eprintln!("Failed to read file {}: {}", filename, error);
+            return;
+        }
+    };
+
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap(); // NOTE: recoverable
+    if let Err(error) = stream.write_all(response.as_bytes()) {
+        eprintln!("Failed to write reponse: {}", error);
+    }
 }
