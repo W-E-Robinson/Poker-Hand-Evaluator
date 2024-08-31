faf02495262d915ee988c63e8a4f55a7e345a2d5
Will Robinson <william.ellis.robinson@gmail.com>
2024-08-31 13:14:32 +0100

    refactor: updated error handling in handle_connection to propogate back up to call

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,18 @@ pub fn web_server(port: String) {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_connection(stream),
+            Ok(stream) => {
+                if let Err(e) = handle_connection(stream) {
+                    eprintln!("Error handling connection: {:?}", e);
+                    // NOTE: how pass an error through stream back to user?
+                }
+            }
             Err(error) => eprintln!("Error accepting connection: {:?}", error),
-            // NOTE: somehow pass up and print recoverable error from handle_connection?
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) { // NOTE: common io Error here can be returned for
-                                              // propagation?
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
     let mut buf_reader = BufReader::new(&mut stream);
 
     let mut headers: Vec<String> = Vec::new();
@@ -29,15 +32,12 @@ fn handle_connection(mut stream: TcpStream) { // NOTE: common io Error here can 
 
     loop {
         let mut line = String::new();
-        buf_reader.read_line(&mut line).unwrap_or_else(|error| {
-            eprintln!("Failed to read line: {:?}", error);
-            return 1; // NOTE: what here? + consider all error handling, can propogate a common
-                      // error up to handle_connection for all these for non recoverable?
-        });
-        line = line.trim().to_string(); // NOTE: is this in place?
+        buf_reader.read_line(&mut line)?;
+        line = line.trim().to_string();
 
         if line.is_empty() {
-            break; // NOTE: how resistant is this? always only one line of space?
+            // Line space between headers and body
+            break;
         }
 
         if let Some(value) = line.strip_prefix("Content-Length: ") {
@@ -48,14 +48,10 @@ fn handle_connection(mut stream: TcpStream) { // NOTE: common io Error here can 
     }
     eprintln!("Headers: {:?}", headers);
 
-    let mut body = vec![0; content_length]; // NOTE: how resistant is this? vec or array?
-    buf_reader.read_exact(&mut body).unwrap_or_else(|error| { // NOTE: how work with the bytes?
-        eprintln!("Failed to read body: {:?}", error);
-        // NOTE: what if GET and no body?
-        return;
-    });
+    let mut body = vec![0; content_length];
+    buf_reader.read_exact(&mut body)?;
 
-    let body_string = String::from_utf8_lossy(&body); // NOTE: what now?
+    let body_string = String::from_utf8_lossy(&body);
     if body_string.len() != 0 {
         eprintln!("Body: {}", body_string);
     }
@@ -65,35 +61,21 @@ fn handle_connection(mut stream: TcpStream) { // NOTE: common io Error here can 
         ("HTTP/1.1 200 OK", "./src/assets/variants.json")
     } else if headers[0].starts_with("POST /evaluate/") {
         eprintln!("Call to: POST /evaluate");
-        let output = evaluate_hands();
-        eprint!("output: {}", output);
         ("HTTP/1.1 200 OK", "./src/assets/variants.json")
     } else {
         eprintln!("Error: Endpoint path not found");
         ("HTTP/1.1 404 NOT FOUND", "./src/assets/404_not_found.json")
     };
 
-    let contents = match fs::read_to_string(filename) {
-        Ok(line) => line,
-        Err(error) => {
-            eprintln!("Failed to read file {}: {:?}", filename, error);
-            return;
-        }
-    };
+    let contents = fs::read_to_string(filename)?;
 
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream
-        .write_all(response.as_bytes())
-        .unwrap_or_else(|error| {
-            eprintln!("Failed to write reponse: {:?}", error);
-            return;
-        });
-    stream.flush().unwrap_or_else(|error| {
-        eprintln!("Failed to flush stream: {:?}", error);
-        return;
-    });
+    stream.write_all(response.as_bytes())?;
+    stream.flush()?;
+
+    Ok(())
 }
 
 fn evaluate_hands() -> String {
