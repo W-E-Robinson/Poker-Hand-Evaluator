7efb20cfb9aea662b34d346796a66dfd1d0c14b5
Will Robinson <william.ellis.robinson@gmail.com>
2025-01-26 19:18:00 +0000

    test: changed integration test folder name to tests, added first failing integration test to docker in shell script

---
+++ run_integration_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker build --pull --no-cache -t poker_hand_evaluator .
+docker run -d -p 8080:8080 --name poker_hand_evaluator poker_hand_evaluator
+
+cargo test
+
+docker stop poker_hand_evaluator
--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,4 @@ fn main() {
     eprintln!("Server successfully starting on port: 8080");
     poker_hand_evaluator::web_server("8080".to_string()); // NOTE: remove the need to put in port
 }
+// why have thread running when can just run Dockerfile in compose and ping against?
