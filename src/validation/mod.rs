5d885589e5dfd678a2fab09901a37b9912c0f995
Will Robinson <william.ellis.robinson@gmail.com>
2026-05-17 15:49:15 +0100

    feat: validate_number_discarded_cards
    validations finished in current form I believe

--- src/validation/mod.rs
+++ src/validation/mod.rs
@@ -1,6 +1,7 @@
 mod validate_no_repeated_cards;
 mod validate_number_board_cards;
 mod validate_number_burn_cards;
+mod validate_number_discarded_cards;
 mod validate_number_player_cards;
 mod validate_number_total_cards;
 mod validate_player_ids;
@@ -9,6 +10,7 @@ use self::{
     validate_no_repeated_cards::validate_no_repeated_cards,
     validate_number_board_cards::validate_number_board_cards,
     validate_number_burn_cards::validate_number_burn_cards,
+    validate_number_discarded_cards::validate_number_discarded_cards,
     validate_number_player_cards::validate_number_player_cards,
     validate_number_total_cards::validate_number_total_cards,
     validate_player_ids::validate_player_ids,
@@ -17,15 +19,13 @@ use self::{
 use crate::types::Hand;
 
 pub fn validate(hand: &Hand) -> Result<(), String> {
-    // NOTE: order here good?
     validate_player_ids(&hand)?;
     validate_number_total_cards(&hand)?;
     validate_no_repeated_cards(&hand)?;
     validate_number_player_cards(&hand)?;
     validate_number_board_cards(&hand)?;
     validate_number_burn_cards(&hand)?;
-    // validate_number_discarded_cards(&hand)?;
-    // validate_number_deck_cards(&hand)?;
+    validate_number_discarded_cards(&hand)?;
 
     Ok(())
 }
---
+++ src/validation/validate_number_discarded_cards.rs
@@ -0,0 +1,115 @@
+use crate::types::{Hand, Variant};
+
+pub fn validate_number_discarded_cards(hand: &Hand) -> Result<(), String> {
+    match hand.variant {
+        Variant::FiveCardDraw => {
+            if hand.discarded_cards.is_none() {
+                return Err(format!(
+                    "There should be discarded cards for {}. There can be zero provided.",
+                    hand.variant.to_string(),
+                ));
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{Card, Player, Rank, Suit};
+
+    use super::*;
+
+    #[test]
+    fn test_no_discarded_cards_given_five_card_draw() {
+        let hand = Hand {
+            id: String::from("hand-id"),
+            variant: Variant::FiveCardDraw,
+            players: vec![Player {
+                id: String::from("player-1-id"),
+                cards: vec![
+                    Card {
+                        rank: Rank::Ace,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::King,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Queen,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Jack,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Ten,
+                        suit: Suit::Heart,
+                    },
+                ],
+            }],
+            burn_cards: Some(vec![Card {
+                rank: Rank::Ace,
+                suit: Suit::Heart,
+            }]),
+            board: None,
+            discarded_cards: None,
+            remaining_deck: vec![Card {
+                rank: Rank::Ace,
+                suit: Suit::Heart,
+            }],
+        };
+
+        let result = validate_number_discarded_cards(&hand).unwrap_err();
+        assert_eq!(
+            result,
+            "There should be discarded cards for Five-card draw. There can be zero provided.",
+        );
+    }
+
+    #[test]
+    fn test_discarded_cards_five_card_draw() {
+        let hand = Hand {
+            id: String::from("hand-id"),
+            variant: Variant::FiveCardDraw,
+            players: vec![Player {
+                id: String::from("player-1-id"),
+                cards: vec![
+                    Card {
+                        rank: Rank::Ace,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::King,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Queen,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Jack,
+                        suit: Suit::Heart,
+                    },
+                    Card {
+                        rank: Rank::Ten,
+                        suit: Suit::Heart,
+                    },
+                ],
+            }],
+            burn_cards: None,
+            board: None,
+            discarded_cards: Some(vec![]),
+            remaining_deck: vec![Card {
+                rank: Rank::Ace,
+                suit: Suit::Heart,
+            }],
+        };
+
+        let result = validate_number_discarded_cards(&hand);
+        assert!(result.is_ok());
+    }
+}
